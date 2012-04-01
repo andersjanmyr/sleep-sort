89eb6dd11aea69ae5a38590ee4197efc9ad2447f
Anders Janmyr <anders@janmyr.com>
2012-04-01 20:32:14 +0200

    Test passing for a single element array

--- test/main.js
+++ test/main.js
@@ -3,17 +3,22 @@ var should = require('should');
 var sleepsort = require('../lib/main');
 
 describe('sleepsort', function() {
-    describe('with no arguments', function() {
-        it('returns an empty array', function() {
-            var result = sleepsort();
-            result.should.eql([]);
-        });
-    });
     describe('with an empty array argument', function() {
-        it('returns an empty array', function() {
-            var result = sleepsort([]);
-            result.should.eql([]);
+        it('calls the callback with an empty array', function(done) {
+            var result = sleepsort([], function(result) {
+                result.should.eql([]);
+                done();
+            });
         });
     });
+    describe('with a single element array', function() {
+        it('calls the callback with a single element array', function(done) {
+            var result = sleepsort([1], function(result) {
+                result.should.eql([1]);
+                done();
+            });
+        });
+    });
+
 });
 
