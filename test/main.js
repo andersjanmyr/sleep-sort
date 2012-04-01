21cca98eb7f480da1e30786aed1e7347aac36113
Anders Janmyr <anders@janmyr.com>
2012-04-01 16:09:53 +0200

    Added a first running test for sleepsort

---
+++ test/main.js
@@ -0,0 +1,13 @@
+"use strict";
+var should = require('should');
+var sleepsort = require('../lib/main');
+
+describe('sleepsort', function() {
+    describe('with no arguments', function() {
+        it('returns an empty array', function() {
+            var result = sleepsort();
+            result.should.eql([]);
+        });
+    });
+});
+
